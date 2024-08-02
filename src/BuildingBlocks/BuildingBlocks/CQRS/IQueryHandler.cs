f47c53757c9181278a53d32a9d8d6e4e2959925f
Sankarshan Ramesh <sankarshanr@gmail.com>
2024-08-02 23:17:32 +0530

    added GetProducts handler and endpoint. Updated docker compose

--- src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -2,6 +2,6 @@
 
 namespace BuildingBlocks.CQRS;
 
-public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TQuery : ICommand<TResponse> where TResponse : notnull
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TQuery : IQuery<TResponse> where TResponse : notnull
 {
 }
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -20,7 +17,10 @@ public class CreateProductHandler : ICommandHandler<CreateProductCommand, Create
             Price = command.Price
         };
 
-        return new CreateProductResult(Guid.NewGuid());
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
     }
 }
 
