61b5815c560f8e817520f1e0d445bf20559382bb
Sankarshan Ramesh <sankarshanr@gmail.com>
2024-10-14 00:02:24 +0530

    added features and repository for basket API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,30 @@
+using BuildingBlocks.Behaviors;
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+//Application Services
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+//Data Services
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
